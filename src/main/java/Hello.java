efa77b10dff68f879840067c2e147617ebf18316
data22 <lowell0322@naver.com>
2022-03-11 15:20:10 +0900

    first

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("hello!! My first Java Coding!!");
+    }
+
+}
+
+
+
+
+//파일 명의 첫글자는 대문자로 설정해야함 , 숫자, 한글 안됨 영어 대문자만, 대소문자 구별해야함
+//main 함수가 있어야 자바 실행됨
