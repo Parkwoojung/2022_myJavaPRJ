5d028f7f957659696efaeb81357bad8853c0b7f2
data22 <lowell0322@naver.com>
2022-03-16 10:34:31 +0900

    변수 선언 int 학습 예제

---
+++ src/main/java/Ex03_07.java
@@ -0,0 +1,11 @@
+//220316 변수 선언 int, float 학습
+
+public class Ex03_07 {
+    public static void main(String[] args) {
+        int a;
+        float b;
+
+        a = (int) 123.45f;
+        b = 200;
+    }
+}
