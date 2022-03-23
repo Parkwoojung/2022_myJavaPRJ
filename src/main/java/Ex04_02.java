ccb36a542d5079b2bd15849acceb28c72f31f91d
data22 <lowell0322@naver.com>
2022-03-23 10:31:47 +0900

    220323_강제 형 변환

--- src/main/java/Ex04_02.java
+++ src/main/java/Ex04_02.java
@@ -8,10 +8,11 @@ public class Ex04_02 {
         result1 = a + b - c;
         System.out.printf("%d + %d - %d = %d \n", a,b,c, result1);
 
-        result1 = a + b * c;
+        result1 = a + b * c; //연산자 우선순위 법칙
         System.out.printf("%d + %d *%d = %d \n", a,b,c, result1);
 
-        result2 = a * b / (float) c;
+        result2 = a * b / (float) c; //강제 형 변환: c앞에 float 로 데이터 타입 변환 => 4에서 4.0으로 변경
+        //실수 4.0과 3이 나눠지면 결과값은 실수로 출력
         System.out.printf("%d * %d / %d = %f \n", a, b, c, result2);
 
         mok  = c / b;
