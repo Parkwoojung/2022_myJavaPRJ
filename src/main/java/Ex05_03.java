334046e02873b019436c4b1969b2e1f9424fafb1
data22 <lowell0322@naver.com>
2022-03-30 10:19:09 +0900

    220330 if else 문

---
+++ src/main/java/Ex05_03.java
@@ -0,0 +1,12 @@
+public class Ex05_03 {
+    public static void main(String[] args) {
+        int a = 200;
+
+        if(a < 100)
+            System.out.printf("100보다 작군요 \n");
+        
+        else
+            System.out.printf("100보다 크군요 \n");
+    }
+}
+//조건문* if는 단독으로 구문 출력이 가능하지만, else는 단독으로 사용하지 못함
