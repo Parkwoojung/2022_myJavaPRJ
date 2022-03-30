07e6617de3c5174f6ddb0dd3f3654c6906444579
data22 <lowell0322@naver.com>
2022-03-30 10:47:59 +0900

    220330 if 중첩문 점수계산

---
+++ src/main/java/Ex05_08.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ex05_08 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+
+        int a;
+
+        System.out.printf("점수를 입력하세요");
+        a = S.nextInt();
+
+        if (a >= 90)
+            System.out.printf("A");
+
+        else
+            if (a >= 80)
+            System.out.printf("B");
+
+        else
+            if (a >= 70)
+            System.out.printf("C");
+
+        else
+            if (a >= 60)
+            System.out.printf("D");
+
+        else
+            System.out.printf("F");
+
+
+        System.out.printf("학점 입니다. \n");
+
+    }
+}
