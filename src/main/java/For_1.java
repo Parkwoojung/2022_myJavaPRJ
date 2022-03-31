2d7cc1e2fef80b7a569aea9198b7aa1bd854701c
data22 <lowell0322@naver.com>
2022-03-31 19:36:50 +0900

    220331 별짓기

--- src/main/java/For_1.java
+++ src/main/java/For_1.java
@@ -3,7 +3,7 @@ public class For_1 {
     public static void main(String[] args) {
 
             //반복문
-          
+
 
                 for (int a = 3; a < 10; a++){
                     System.out.println(a+"단");
