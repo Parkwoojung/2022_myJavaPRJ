2ae69fdbc4aaddca9c3e4e12cafc61a430df009a
data22 <lowell0322@naver.com>
2022-03-31 19:38:44 +0900

    220331 지역 변수, 전역 변수

---
+++ src/main/java/PublicPrivate.java
@@ -0,0 +1,22 @@
+public class PublicPrivate {
+
+        class Test{
+            int c =1; //지역변수
+        }
+
+            //c=2;  지역변수 개념
+            int a = 10;
+            int b = 20;
+            int result = a + b; //전역변수
+
+            for(int x=0;x<10;x++){
+                result=12; //전역변수
+                int v=1; //지역변수
+            }
+            //v=2;지역변수
+
+            System.out.printf("result: %d \n", result);
+        }
+
+
+
