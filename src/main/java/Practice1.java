e752e77a575928daa27a46b6eb399c56dca49547
data22 <lowell0322@naver.com>
2022-04-01 15:51:36 +0900

    220401 if 문 100보다 작군요

---
+++ src/main/java/Practice1.java
@@ -0,0 +1,13 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        //if 문 예제
+
+        int a = 200;
+
+        if (a<100)
+            System.out.printf("100보다 작군요..\n");
+            System.out.printf("거짓이므로 이 문장은 안 보이겠죠? \n");
+
+        System.out.printf("프로그램 끝! \n");
+    }
+}
