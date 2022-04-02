f40b49886ad056b1789c077dd075cc344735418e
data22 <lowell0322@naver.com>
2022-04-02 19:58:46 +0900

    220402 빙글빙글 for문을 공부 중입니다^^

---
+++ src/main/java/Practice4.java
@@ -0,0 +1,11 @@
+public class Practice4 {
+    public static void main(String[] args) {
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+        System.out.printf("안녕하세요? 빙글빙글 for문을 공부중입니다. ^^\n");
+
+    }
+}
--- src/main/java/PublicPrivate.java
+++ src/main/java/PublicPrivate.java
@@ -9,14 +9,14 @@ public class PublicPrivate {
             int b = 20;
             int result = a + b; //전역변수
 
-            for(int x=0;x<10;x++){
-                result=12; //전역변수
-                int v=1; //지역변수
+            //for(int x=0;x<10;x++){
+               // result=12; //전역변수
+                //int v=1; //지역변수
             }
             //v=2;지역변수
 
-            System.out.printf("result: %d \n", result);
-        }
+            //System.out.printf("result: %d \n", result);
+
 
 
 
