9843db25b9bb21d25fa00d610f362c831e726556
data22 <lowell0322@naver.com>
2022-04-06 10:45:27 +0900

    220406 for문으로 1~10까지의 합 계산

---
+++ src/main/java/Ex06_088.java
@@ -0,0 +1,14 @@
+public class Ex06_088 {
+    public static void main(String[] args) {
+        int hap = 0;
+        int i;
+
+        for (i = 1; i < 11; i++){
+            hap +=1;
+            System.out.println("hap 변수 값: " +hap);
+            System.out.println("i 변수 값: " +i);
+            System.out.println("-------------");
+        }
+        System.out.println("1부터 10까지의 합은" +hap);
+    }
+}
