80244a0f375ba6e82ae26ba3868e5cd1c2f9de15
data22 <lowell0322@naver.com>
2022-04-12 22:32:37 +0900

    220412 중첩 for문 별찍기 10*10

--- src/main/java/Wjstar03.java
+++ src/main/java/Wjstar03.java
@@ -1,7 +1,10 @@
 public class Wjstar03 {
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++){
-            System.out.print("*");
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
     }
 }
