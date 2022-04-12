5c0ca4270308e18a220e9b6157d45149db3ff025
data22 <lowell0322@naver.com>
2022-04-12 20:11:58 +0900

    220412 p.195 06_02 for문

---
+++ src/main/java/Wjfor2.java
@@ -0,0 +1,9 @@
+public class Wjfor2 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 5; i++) {
+            System.out.printf("안녕하세요? 빙글 for문을 공부중입니다. \n");
+        }
+    }
+}
