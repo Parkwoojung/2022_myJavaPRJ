7ce4bc5556fca16ccddcc04c0938a4cab251baf0
data22 <lowell0322@naver.com>
2022-04-12 21:40:58 +0900

    220412 p.206 합

---
+++ src/main/java/Wjhap.java
@@ -0,0 +1,11 @@
+public class Wjhap {
+    public static void main(String[] args) {
+        int hap = 0;
+
+        for (int i = 501; i <= 1000; i++) {
+            hap += i;
+            System.out.printf("500에서 1000까지의 홀수의 합: %d \n", hap);
+        }
+    }
+
+}
