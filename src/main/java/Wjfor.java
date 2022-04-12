7a2cb6438ef73e2e6c13595d78dde2064fbf82f5
data22 <lowell0322@naver.com>
2022-04-12 20:06:07 +0900

    220412 p.194 06_01

---
+++ src/main/java/Wjfor.java
@@ -0,0 +1,10 @@
+public class Wjfor {
+    public static void main(String[] args) {
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+        System.out.printf("안녕하세요 빙글 for문을 공부 중입니다. ^^\n");
+    }
+}
