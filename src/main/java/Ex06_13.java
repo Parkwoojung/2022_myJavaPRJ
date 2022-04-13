c92f85e52fd765b0cfaf142ccf32394a13c3f667
data22 <lowell0322@naver.com>
2022-04-13 10:02:11 +0900

    220413 Ex06_13 중첩 for문

---
+++ src/main/java/Ex06_13.java
@@ -0,0 +1,9 @@
+public class Ex06_13 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.printf("중첩 for문 입니다. (i값: %d, k값: %d)\n ", i, j);
+            }
+        }
+    }
+}
