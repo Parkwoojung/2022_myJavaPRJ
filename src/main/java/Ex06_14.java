a6d61548a5dacf08c90fd874d4575f2b15659586
data22 <lowell0322@naver.com>
2022-04-13 10:21:33 +0900

    220413 for문 구구단

---
+++ src/main/java/Ex06_14.java
@@ -0,0 +1,12 @@
+public class Ex06_14 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i++ ){
+            for (int k = 1; k < 10; k++){
+                System.out.printf("%d X %d = %d \n", i, k, i*k);
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
