7aa1e8be6f3a87511366d29de2e89c8112c3a709
data22 <lowell0322@naver.com>
2022-04-18 23:26:25 +0900

    220418 변수와 데이터 형식_3

---
+++ src/main/java/Test03.java
@@ -0,0 +1,9 @@
+public class Test03 {
+    public static void main(String[] args) {
+        //변수와 데이터 형식 3
+        System.out.printf("%d", 100, 200); //%d는 1개, 숫자 2개
+        System.out.printf("\n");
+        System.out.printf("%d %d", 100); //%d는 두 개 숫자는 1개
+        System.out.printf("\n");
+    }
+}
