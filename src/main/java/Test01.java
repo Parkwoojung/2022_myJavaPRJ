9564090fbae8d50a369230e2636db5f03f18ac13
data22 <lowell0322@naver.com>
2022-04-18 22:42:32 +0900

    220418 변수와 데이터 형식_1

---
+++ src/main/java/Test01.java
@@ -0,0 +1,13 @@
+public class Test01 {
+    public static void main(String[] args) {
+        //03변수와 데이터 형식
+        System.out.println("안녕하세요?");
+        System.out.println("JAVA입니다.");
+
+        System.out.printf("안녕하세요?"); //숫자 100이 아닌 문자 일영영
+        System.out.printf("100"); //숫자 100이 아닌 문자 일영영
+
+        System.out.printf("%d", 100); //%d는 숫자를 서직하기 때문에 100으로 인식함
+    }
+}
+
