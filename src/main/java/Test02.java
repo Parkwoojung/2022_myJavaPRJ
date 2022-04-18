ba3e97520cc09aa086d8623dfc1d81020f3f34e1
data22 <lowell0322@naver.com>
2022-04-18 23:21:39 +0900

    220418 변수와 데이터 형식_2

---
+++ src/main/java/Test02.java
@@ -0,0 +1,10 @@
+public class Test02 {
+    public static void main(String[] args) {
+        //변수와 데이터 형식 2
+
+        System.out.printf("100+100"); //모두 글자로 취급
+        System.out.printf("\n"); //printf는 행이 넘어가지 않음 강제 행 넘어가도록
+        System.out.printf("%d", 100 + 100); //숫자를 계산헤서 출력함 %d
+        System.out.printf("\n"); //행 넘어감
+    }
+}
