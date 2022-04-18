d461c6d89183fc5725b0f55d7b6dc2f32b69c1f5
data22 <lowell0322@naver.com>
2022-04-18 23:52:10 +0900

    220418 변수와 데이터 형식_4

---
+++ src/main/java/Test04.java
@@ -0,0 +1,8 @@
+public class Test04 {
+    public static void main(String[] args) {
+        //변수와 데이터 형식_4
+        System.out.printf("%d / %d = %f \n", 100, 200, 0.5); //%d정수 2개 %s 실수 한 개
+        System.out.printf("%c %c \n", 'a', 'k'); //문자 2개 출력 %c
+        System.out.printf("%s %s \n", "IT CookBook", "JAVA"); //문자열 %s 2개 출력
+    }
+}
