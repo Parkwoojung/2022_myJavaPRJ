5a5736ef76eb8dc9b07b2e1d22741c0c5275bf8d
data22 <lowell0322@naver.com>
2022-04-18 23:50:25 +0900

    220418 변수와 데이터 형식_6

---
+++ src/main/java/Test06.java
@@ -0,0 +1,11 @@
+public class Test06 {
+    public static void main(String[] args) {
+        //데이터 형식과 배열
+        int a = 100, b = 200; //변수 a는 100, b는 200 지정
+        float result; //실수형 변수 result 선언
+
+        result = a * b;
+        System.out.printf("%f \n", result);
+
+    }
+}
