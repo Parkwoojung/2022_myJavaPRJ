a24f203d3b48f0cee7efbadcfaeacd4454c86b80
data22 <lowell0322@naver.com>
2022-04-20 13:06:40 +0900

    220420 a++

---
+++ src/main/java/Test22.java
@@ -0,0 +1,14 @@
+public class Test22{
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        a++;
+        System.out.println(a);
+
+        ++a;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(++a);
+    }
+}
