e709eed91d21a813c22d82c7dff2205656a72c75
data22 <lowell0322@naver.com>
2022-04-21 09:42:35 +0900

    220421 중첩 for 문 안녕!

---
+++ src/main/java/Test20.java
@@ -0,0 +1,13 @@
+public class Test20 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("안녕 \n");
+            System.out.printf("또 안녕 \n");
+        }
+        System.out.printf("\n \n");
+
+        for (int i = 0; i < 3; i++)
+            System.out.printf("안녕? \n");
+        System.out.printf("또 안녕? \n");
+    }
+}
