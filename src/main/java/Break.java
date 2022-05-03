a37d5351350cb10065ef2f1f4dcafd6242b5d6ac
data22 <lowell0322@naver.com>
2022-05-03 17:53:36 +0900

    220503 Break문

---
+++ src/main/java/Break.java
@@ -0,0 +1,18 @@
+public class Break {
+    public static void main(String[] args) {
+
+        Loop1:
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (j == 1) {
+//                    break Loop1;
+                    break;
+//                    continue Loop1;
+//                    continue;
+                }
+            }
+            System.out.println("그냥 break!!!!");
+        }
+        System.out.println("이름 붙힌 break, continue!!");
+    }
+}
