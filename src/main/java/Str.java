86e538198f54c92944cab53711e3da392b97127b
data22 <lowell0322@naver.com>
2022-05-09 22:19:48 +0900

    220509 str문자열

---
+++ src/main/java/Str.java
@@ -0,0 +1,10 @@
+public class Str {
+    public static void main(String[] args) {
+        String str ="내일은 맛있는 학식이 나오면 좋겠는데용..과연 무엇이 나올까요?";
+
+        int len = str.length();
+
+        System.out.printf("문자열: %s \n", str);
+        System.out.printf("문자열 길이: %d", len);
+    }
+}
