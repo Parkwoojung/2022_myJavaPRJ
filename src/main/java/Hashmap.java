ec4d70e1589a7214eed5759f3adcdd8ecf78f212
data22 <lowell0322@naver.com>
2022-05-12 21:26:48 +0900

    220512 hashmap

---
+++ src/main/java/Hashmap.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmap {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "박우정");
+        map.put("email", "lowell0322@naver.com");
+        map.put("dept", "데이터분석과");
+
+        System.out.println("--------------------------");
+
+        System.out.println("name: "+map.get("name"));
+        System.out.println("email: "+map.get("email"));
+        System.out.println("dept: "+map.get("dept"));
+
+        System.out.println("--------------------------");
+
+    }
+}
