cbdbf8a77f6acb6296245b8b7e4853e7f0b86ba5
data22 <lowell0322@naver.com>
2022-05-23 19:12:09 +0900

    220523 문자열, 문자열길이

---
+++ src/main/java/Java_0518_2.java
@@ -0,0 +1,10 @@
+public class Java_0518_2 {
+    public static void main(String[] args) {
+        String str = "IT CookBook.Java";
+        int len = str.length(); //length 문자열 길이, 중복
+
+        System.out.printf("문자열: %s \n", str);
+        System.out.printf("문자열 길이: %d", len);
+
+    }
+}
