e304a0d021ea766f120449ed4556676277588d1d
data22 <lowell0322@naver.com>
2022-06-15 23:55:15 +0900

    220615 부모 자식 클래스 상속 개념

---
+++ src/main/java/STUDY/Java_0615_3.java
@@ -0,0 +1,50 @@
+package STUDY;
+
+class Car {
+    String color;
+    int speed;
+
+    void upSpeed(int value){
+        System.out.println("속도를 올립니다.");
+        speed = speed + value;
+    }
+
+    void downSpeed(int value){
+        speed = speed - value;
+    }
+}
+
+class Sedan extends Car { //부모의 car클래스를 상속받겠다. car클래스는 위에 있는 클래스
+    int seatNum;
+
+    int getSeatNum(){
+        return seatNum;
+    }
+}
+
+class Truck extends Car {
+    int capacity;
+
+    int getCapacity(){
+        return capacity;
+    }
+}
+
+public class Java_0615_3{
+    public static void main(String[] args) {
+
+        Sedan sedan1 = new Sedan();
+        Truck truck1 = new Truck();
+
+        sedan1.upSpeed(300);
+        truck1.upSpeed(100);
+
+        sedan1.seatNum = 5;
+        truck1.capacity = 50;
+
+        System.out.println("승용차 속도는" + sedan1.speed+ "km, 좌석수는 "+ sedan1.getSeatNum() +"개 입니다.");
+        System.out.println("트럭 속도는" + truck1.speed+"km,적재량은"+ truck1.getCapacity()+"톤입니다.");
+    }
+}
+
+
